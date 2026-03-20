c40b662fd5ec325aca600a1e71fa00ba05482bc5
Aungpyih31n <aungpyih31n@gmail.com>
2026-03-20 13:48:50 +0630

    DB

--- POS.Api/obj/Debug/net8.0/POS.Api.AssemblyInfo.cs
+++ POS.Api/obj/Debug/net8.0/POS.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6d3b0700e7bdebe30539408397d29c2cdc4d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- POS.Core/obj/Debug/net8.0/POS.Core.AssemblyInfo.cs
+++ POS.Core/obj/Debug/net8.0/POS.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6d3b0700e7bdebe30539408397d29c2cdc4d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
